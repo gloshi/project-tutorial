472c63ca9c5bc61463ceca47d3b12d53d74b9239
gloshi <sasha.komarov.90@inbox.ru>
2023-05-17 22:04:18 +0300

    Added build

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,35 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resourcePath: string) => resourcePath.includes(".module"),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
+  return [typescriptLoader, cssLoader];
+}
